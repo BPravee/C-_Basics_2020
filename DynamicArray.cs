81d42096137dd321956b4b6b7d6c7bd012eb2b89
venu-shastri <venu.shastri@gmail.com>
2020-04-23 11:56:45 +0530

    Add files via upload

---
+++ DynamicArray.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+   public class DynamicArray
+    {
+        //buffer
+        int[] buffer = new int[10];
+
+        //public void Set_Item(int index,int value)
+        //{
+        //    if(index >= this.buffer.Length)
+        //    {
+        //        //OverFlow
+        //        //Create temp buffer
+        //        int[] tempBuffer = new int[this.buffer.Length + index];
+        //        //Copy Items from Old Buffer to New Buffer
+        //        for(int i = 0; i < buffer.Length; i++)
+        //        {
+        //            tempBuffer[i] = buffer[i];
+        //        }
+        //        //Change Buffer Address
+        //        this.buffer = tempBuffer;
+        //    }
+        //    this.buffer[index] = value;
+        //}
+
+        //public int Get_Item(int index)
+        //{
+
+        //    return this.buffer[index];
+
+        //}
+
+        #region Indexer - Allows indexing an object
+        public int this[int index]
+        {
+            get { return this.buffer[index]; }
+            set {
+                if (index >= this.buffer.Length)
+                {
+                    //OverFlow
+                    //Create temp buffer
+                    int[] tempBuffer = new int[this.buffer.Length + index];
+                    //Copy Items from Old Buffer to New Buffer
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        tempBuffer[i] = buffer[i];
+                    }
+                    //Change Buffer Address
+                    this.buffer = tempBuffer;
+                }
+                this.buffer[index] = value;
+            }
+        }
+        #endregion
+    }
+}
---
+++ PatientInfoDataModel.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+    public class PatientInfoDataModel
+    {
+        #region Private Data Members (Fields) , state - allocated in Heap
+        private string _mrn;
+        private string _name;
+        private string _email;
+        private string _phoneNumber;
+        #endregion
+
+        #region Constructors
+        //Constructor is a function
+        //Constructor Name is same as TypeName
+        //Constructor By Default return void (implicit)
+        //constructor can  be overloaded
+        //constructor can call another contructor
+
+            //Default Constructor - Argument Lenght = 0
+        public PatientInfoDataModel()
+        {
+            System.Random _randomgenerator = new Random();
+         //  string autoMrn = "MRN" + _randomgenerator.Next(1, 1000);
+         //New Grammer String Interpollation - Dontnet Fx 4.5 + 
+         string automMrn= $"MRN{_randomgenerator.Next()}";
+
+            this._mrn = automMrn;
+            this._name = default(string);
+            this._email = default(string);
+            this._phoneNumber = default(string);
+
+        }
+
+        public PatientInfoDataModel(string mrn,string name,string contactNumber):this()
+        {
+            this._mrn = mrn;
+            this._name = name;
+            this._phoneNumber = contactNumber;
+        }
+
+        public PatientInfoDataModel(string mrn,string name,string email,string contactNumber):this(mrn,name,contactNumber)
+        {
+            this._email = email;
+           
+        }
+        #endregion
+
+        #region Mutators / Methods
+
+        //public string Get_mrn()
+        //{
+        //    return this._mrn;
+
+        //}
+        //public void Set_email(string value)
+        //{
+        //    this._email = value;
+        //}
+        //public string Get_email() { return this._email;  }
+
+        #endregion
+
+        #region Properties - smart data Fields
+        public string MRN
+        {
+            get { return this._mrn; }
+        }
+        public string Name
+        {
+            get { return this._name; }
+            set { this._name = value; }
+        }
+        public string Email
+        {
+            get { return this._email; }
+            set { this._email = value; }
+        }
+
+        public string ContactNumber
+        {
+            get { return this._phoneNumber; }
+            set { this._phoneNumber = value; }
+        }
+        #endregion
+    }
+}
