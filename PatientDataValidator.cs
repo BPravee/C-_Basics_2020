1f350177f5c4f0e804b12461e9e99db6991fe0a4
venu-shastri <venu.shastri@gmail.com>
2020-04-24 11:29:26 +0530

    Add files via upload

---
+++ PatientDataValidator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentOne
+{
+   public  class PatientDataValidator
+    {
+        ConsoleLogger _logger = new ConsoleLogger();
+        public bool Validate(string mrn,string email,string name,string contactNumber)
+        {
+            if (string.IsNullOrEmpty(mrn))
+            {
+                //Log
+                _logger.Write("MRN Invalid");
+            }
+            return false;
+
+        }
+            
+    }
+}
