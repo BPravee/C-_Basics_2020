780c13ca79283292a5b5c7f0ae056fd7425e72c3
venu-shastri <venu.shastri@gmail.com>
2020-04-25 12:15:10 +0530

    Add files via upload

---
+++ AttributeDemo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeDemo
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class TestAttribute:System.Attribute
+    {
+        string name;
+        public TestAttribute() { }
+        public TestAttribute(string name)
+        {
+            this.name = name;
+        }
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public void Fun() { }
+    }
+
+    [TestAttribute("Tom",Name ="Jack")] //Will not invoke Constructor
+    class A
+    {
+        [TestAttribute("Hary",Name ="James")]
+        public void Fun() { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //refers TypeDef of A from metadata
+           System.Type _typeDef= typeof(A);
+            //Deserializes TestAttribute Objects From target Metadata
+        TestAttribute[] testAttributes  = 
+                _typeDef.GetCustomAttributes(typeof(TestAttribute), true) as TestAttribute[];
+
+          TestAttribute instance=  testAttributes[0];
+            Console.WriteLine(instance.Name);
+
+           System.Reflection.MethodInfo method= _typeDef.GetMethod("Fun");
+            testAttributes = method.GetCustomAttributes(typeof(TestAttribute), true) as TestAttribute[];
+                instance = testAttributes[0];
+            Console.WriteLine(instance.Name);
+
+
+
+        }
+    }
+}
---
+++ Keywords.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyWords
+{
+    public delegate void TestDelegate();
+    class Program
+    {
+        static void Main_old(string[] args)
+        {
+            int data = 10;
+            Echo(ref data);
+            Console.WriteLine(data);
+
+            int targetValue;
+            bool isValidConvertion=ConvertToInteger("23", out targetValue);
+            Console.WriteLine(targetValue);
+
+            int[] numbers = { 1, 2, 3 };
+            Add( out targetValue, numbers);
+            //ReInitialize Array
+            numbers = new int[] { 23, 4, 5, 6, 6, 7, 7 };
+            Add( out targetValue, numbers);
+
+            Add(out targetValue, 2, 3);
+            Add(out targetValue,3, 5, 6, 6, 7, 7, 8);
+            Add(out targetValue,2, 3, 3, 4, 5, 5, 6, 6,7, 7, 9);
+
+        }
+        static void Echo(ref int x)
+        {
+            x = 100;
+
+        }
+        static bool ConvertToInteger(string data,out int convertedValue)
+        {
+            convertedValue = Int32.Parse(data);
+            return true;
+        }
+        public static void Add(out int sum, params int[] numbers)
+        {
+            sum = default(int);
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+        }
+        public static void Main()
+        {
+            TestDelegate obj = () => {
+                Console.WriteLine("Hello");
+            };
+            /*
+             * void  <Main_b__4_0(){Console.WriteLine("Hello") ; }
+             * TestDelegate obj=new TestDelegate(Program.<Main_b__4_0>);
+             * 
+             * 
+             */
+            obj.Invoke();
+        }
+    }
+}
